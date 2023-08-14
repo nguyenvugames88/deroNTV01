83c626cfc2b8adefa54b6e1c6911bcc01d4f9c9f
nguyenvugames88 <110708941+nguyenvugames88@users.noreply.github.com>
2023-08-15 03:13:25 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qy8q9dwj922ge7q54nwz02uqjwcn372958phvyerdtkxuhsgdczk7qg7f742p -r dero.rabidmining.com:10300 -r1 dero.friendspool.club:10300 rpc;
+    ./astrominer -w dero1qy8q9dwj922ge7q54nwz02uqjwcn372958phvyerdtkxuhsgdczk7qg7f742p -r dero.rabidmining.com:10300 -r1 dero.friendspool.club:10300 -m 7 rpc;
     sleep 5;
 done
